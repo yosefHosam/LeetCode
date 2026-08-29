fdfa8ad9d0377787c7b900268cfae0940bc6cd42
yosefHosam <youssefhhossam1@gmail.com>
2026-08-29 19:44:48 +0300

    Time: 23 ms (53.3%), Space: 66.9 MB (70.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cs
+++ 0049-group-anagrams/0049-group-anagrams.cs
@@ -1,22 +1,22 @@
-public class Solution {
-    public IList<IList<string>> GroupAnagrams(string[] strs) {
-        var map = new Dictionary<string, List<string>>();
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> anagramGroups = new Dictionary<string, List<string>>();
 
-        foreach (var s in strs) {
-            int[] count = new int[26];
-            foreach (char c in s) {
-                count[c - 'a']++;
-            }
+        foreach (string str in strs)
+        {
 
-            string key = string.Join(",", count);
+            char[] chars = str.ToCharArray();
+            Array.Sort(chars);
+            string sortedKey = new string(chars);
 
-            if (!map.ContainsKey(key)) {
-                map[key] = new List<string>();
-            }
+            if (!anagramGroups.ContainsKey(sortedKey))
+                anagramGroups[sortedKey] = new List<string>();
 
-            map[key].Add(s);
+            anagramGroups[sortedKey].Add(str);
         }
 
-        return map.Values.ToList<IList<string>>();
+        return new List<IList<string>>(anagramGroups.Values);
     }
 }
