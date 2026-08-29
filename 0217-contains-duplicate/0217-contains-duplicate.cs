98f9931cb3ae9d67770f5677ba73b1784c99e0bc
yosefHosam <youssefhhossam1@gmail.com>
2026-08-29 19:45:56 +0300

    Time: 13 ms (86.94%), Space: 74.2 MB (71.77%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -0,0 +1,14 @@
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (!set.Add(num)) 
+                return true;
+        }
+        return false;
+
+    }
+}
