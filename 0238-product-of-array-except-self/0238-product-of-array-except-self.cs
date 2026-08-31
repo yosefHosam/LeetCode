eefe15dcfd11c7511a5b8ce20f56b72518548b9f
yosefHosam <youssefhhossam1@gmail.com>
2026-08-31 11:30:16 +0300

    Time: 1 ms (100%), Space: 62.6 MB (80.1%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -4,19 +4,19 @@ public class Solution
     {
         int n = nums.Length;
         int[] result = new int[n];
-        
-        result[0] = 1;
-        for (int i = 1; i < n; i++)
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++)
         {
-            result[i] = result[i - 1] * nums[i - 1];
+            result[i] = prefix;
+            prefix *= nums[i];
         }
 
-        int rightProduct = 1;
+        int postfix = 1;
         for (int i = n - 1; i >= 0; i--)
         {
-            result[i] = result[i] * rightProduct;
-            
-            rightProduct = rightProduct * nums[i];
+            result[i] *= postfix;
+            postfix *= nums[i];
         }
 
         return result;
