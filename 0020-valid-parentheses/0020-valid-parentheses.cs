ef6dd367e2c9ae42672a31d1eece8c0d84982380
yosefHosam <youssefhhossam1@gmail.com>
2026-09-03 12:26:54 +0300

    Time: 2 ms (94.74%), Space: 41.5 MB (97.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                stack.Push(s[i]);
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                char top = stack.Peek();
+                if ((s[i] == ')' && top == '(') || (s[i] == '}' && top == '{') || (s[i] == ']' && top == '['))
+                    stack.Pop();
+                else
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
