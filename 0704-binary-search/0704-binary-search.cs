c025add3a60540b24db8c4c6095b1c85d0abe039
yosefHosam <youssefhhossam1@gmail.com>
2026-09-07 22:19:36 +0300

    Time: 0 ms (100%), Space: 52.8 MB (69.09%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            // fix Integer Overflow
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1; // Target not found
+    }
+}
